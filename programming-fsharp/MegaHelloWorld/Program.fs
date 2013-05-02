20cede6a890e58a505c1ae433b7b4a0ba058eb46
unknown <martin.christopher.s@gmail.com>
2013-05-02 01:19:31 -0700

    Added Programming F# samples

---
+++ programming-fsharp/MegaHelloWorld/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+
+(*
+    MegaHelloWorld
+    Take two command line arguments
+    Print them to the console with the current date and time
+*)
+
+open System
+
+[<EntryPoint>]
+let main (args: string[]) = 
+    if args.Length <> 2 then
+        failwith "Error: Expected arguments <greeting> and <thing>"
+    let greeting, thing = args.[0], args.[1]
+    let timeOfDay =  DateTime.Now.ToString("hh:mm tt")
+    
+    printfn "%s, %s at %s" greeting, thing, timeOfDay
+    
+    //program exit code
+    0
